adc54b798da2ec8c59625b331bc9e32d51edd5ac
yb-fboese <fabio.boese@whybe.com.br>
2024-03-04 19:52:34 -0300

    version 1 published

---
+++ src/YTDownloader/YTDownloader/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace YTDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnDownloadSlow = new System.Windows.Forms.Button();
+            this.btnDownloadFast = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Link:";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLink.Location = new System.Drawing.Point(59, 14);
+            this.txtLink.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(670, 26);
+            this.txtLink.TabIndex = 3;
+            this.txtLink.TextChanged += new System.EventHandler(this.txtLink_TextChanged);
+            // 
+            // txtId
+            // 
+            this.txtId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(59, 50);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(232, 26);
+            this.txtId.TabIndex = 3;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID:";
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pbProgress.Location = new System.Drawing.Point(0, 249);
+            this.pbProgress.MarqueeAnimationSpeed = 5;
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(844, 12);
+            this.pbProgress.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pbProgress.TabIndex = 4;
+            this.pbProgress.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDownloadSlow);
+            this.panel1.Controls.Add(this.btnDownloadFast);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtLink);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(844, 105);
+            this.panel1.TabIndex = 5;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(0, 105);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(844, 144);
+            this.txtLog.TabIndex = 6;
+            this.txtLog.WordWrap = false;
+            // 
+            // btnDownloadSlow
+            // 
+            this.btnDownloadSlow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownloadSlow.Enabled = false;
+            this.btnDownloadSlow.Image = global::YTDownloader.Properties.Resources.download_solid__1__h24;
+            this.btnDownloadSlow.Location = new System.Drawing.Point(541, 50);
+            this.btnDownloadSlow.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDownloadSlow.Name = "btnDownloadSlow";
+            this.btnDownloadSlow.Size = new System.Drawing.Size(188, 46);
+            this.btnDownloadSlow.TabIndex = 0;
+            this.btnDownloadSlow.Text = "Baixar (lento)";
+            this.btnDownloadSlow.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDownloadSlow.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDownloadSlow.UseVisualStyleBackColor = true;
+            this.btnDownloadSlow.Click += new System.EventHandler(this.btnDownloadSlow_Click);
+            // 
+            // btnDownloadFast
+            // 
+            this.btnDownloadFast.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownloadFast.Enabled = false;
+            this.btnDownloadFast.Image = global::YTDownloader.Properties.Resources.download_solid_h24;
+            this.btnDownloadFast.Location = new System.Drawing.Point(345, 50);
+            this.btnDownloadFast.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDownloadFast.Name = "btnDownloadFast";
+            this.btnDownloadFast.Size = new System.Drawing.Size(188, 46);
+            this.btnDownloadFast.TabIndex = 0;
+            this.btnDownloadFast.Text = "Baixar (rápido)";
+            this.btnDownloadFast.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDownloadFast.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDownloadFast.UseVisualStyleBackColor = true;
+            this.btnDownloadFast.Click += new System.EventHandler(this.btnDownloadFast_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::YTDownloader.Properties.Resources.gabi;
+            this.pictureBox1.Location = new System.Drawing.Point(737, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 261);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GABI YT Downloader";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownloadFast;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnDownloadSlow;
+    }
+}
+
---
+++ src/YTDownloader/YTDownloader/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+using YoutubeExplode;
+
+namespace YTDownloader
+{
+    public partial class Form1 : Form
+    {
+        private bool busy = false;
+        private bool success = false;
+        private string destinationFile = string.Empty;
+        private Exception downloadException = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void SlowDownload()
+        {
+            try
+            {
+                var VideoUrl = $"https://www.youtube.com/embed/{txtId.Text}.avi";
+                var youTube = YouTube.Default;
+                var video = youTube.GetVideo(VideoUrl);
+                System.IO.File.WriteAllBytes(destinationFile, video.GetBytes());
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                downloadException = ex;
+            }
+            busy = false;
+        }
+
+        private void FastDownload()
+        {
+            var youtube = new YoutubeClient();
+            var video = youtube.Videos.GetAsync(txtLink.Text).GetAwaiter().GetResult();
+
+            // Get all available muxed streams
+            var streamManifest = youtube.Videos.Streams.GetManifestAsync(video.Id).GetAwaiter().GetResult();
+            var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
+
+            if (muxedStreams.Any())
+            {
+                var streamInfo = muxedStreams.First();
+                using (var httpClient = new HttpClient())
+                {
+                    var stream = httpClient.GetStreamAsync(streamInfo.Url).GetAwaiter().GetResult();
+                    var datetime = DateTime.Now;
+
+                    using (var outputStream = File.Create(destinationFile))
+                    {
+                        stream.CopyToAsync(outputStream).GetAwaiter().GetResult();
+                    }
+                    success = true;
+                }
+            }
+            else
+            {
+                downloadException = new Exception($"No suitable video stream found for {video.Title}.");
+            }
+            busy = false;
+        }
+
+        private void LogError(string message)
+        {
+            txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] ### ERRO ###: {message}\r\n");
+            txtLog.SelectionStart = txtLog.Text.Length;
+        }
+        private void LogProgress(string message)
+        {
+            txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] --- INFO ---: {message}\r\n");
+            txtLog.SelectionStart = txtLog.Text.Length;
+        }
+
+        private string ExtractId(string link)
+        {
+            var regex = new System.Text.RegularExpressions.Regex(@"(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?![\w-])");
+            var match = regex.Match(link);
+            return match.Success ? match.Groups[1].Value : string.Empty;
+        }
+
+        private bool SelectDestinationFile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "AVI Files|*.avi";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                destinationFile = sfd.FileName;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        private void ShowDownloadResults()
+        {
+            if (success)
+                LogProgress($"Download concluído com sucesso para o arquivo {destinationFile}");
+            else
+            {
+                if (downloadException != null)
+                    LogError(downloadException.Message);
+                LogError("Falha ao realizar o download do vídeo");
+            }
+        }
+
+        private void RunDownloadInBackground(ThreadStart downloadAction)
+        {
+            LogProgress($"Realizando o download do vídeo para o arquivo {destinationFile}");
+            System.Threading.Thread thread = new System.Threading.Thread(downloadAction);
+            thread.Start();
+            while (busy)
+            {
+                Application.DoEvents();
+                Thread.Sleep(500);
+            }
+        }
+
+        private void SetIdleView()
+        {
+            panel1.Enabled = true;
+            pbProgress.Visible = false;
+        }
+
+        private void SetBusyView()
+        {
+            pbProgress.Visible = true;
+            panel1.Enabled = false;
+            success = false;
+            busy = true;
+        }
+
+        private void txtLink_TextChanged(object sender, EventArgs e)
+        {
+            txtId.Text = ExtractId(txtLink.Text);
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+            btnDownloadFast.Enabled = txtLink.Text != string.Empty;
+            btnDownloadSlow.Enabled = txtId.Text != string.Empty;
+        }
+
+        private void btnDownloadFast_Click(object sender, EventArgs e)
+        {
+            if (SelectDestinationFile())
+            {
+                SetBusyView();
+                RunDownloadInBackground(FastDownload);
+                ShowDownloadResults();
+                SetIdleView();
+            }
+        }
+
+        private void btnDownloadSlow_Click(object sender, EventArgs e)
+        {
+            if (SelectDestinationFile())
+            {
+                SetBusyView();
+                RunDownloadInBackground(SlowDownload);
+                ShowDownloadResults();
+                SetIdleView();
+            }
+        }
+    }
+}
